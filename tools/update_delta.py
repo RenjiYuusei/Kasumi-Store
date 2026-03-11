ed6793293b4d912ef419a1ee9afd1725c2df6adc
Renji Yuusei <166010224+RenjiYuusei@users.noreply.github.com>
2026-03-11 15:26:29 +0700

    Update update_delta.py

--- tools/update_delta.py
+++ tools/update_delta.py
@@ -15,8 +15,8 @@ APPS_JSON_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # VSPhone Credentials (Hardcoded)
-VSPHONE_USER = "eligible1827@buzzcut.ws"
-VSPHONE_PASS = "935826"
+VSPHONE_USER = "shallow9210@whitehousecalculator.com"
+VSPHONE_PASS = "871985"
 
 DEFAULT_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
