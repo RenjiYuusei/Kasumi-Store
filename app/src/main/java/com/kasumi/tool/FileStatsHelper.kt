c1bf0973bceae0252c28c78e7c2354cad804fe5e
RenjiYuusei <166010224+RenjiYuusei@users.noreply.github.com>
2026-03-27 15:44:53 +0000

    Optimize updateItemFileStats by omitting file.exists() check on newer APIs

--- app/src/main/java/com/kasumi/tool/FileStatsHelper.kt
+++ app/src/main/java/com/kasumi/tool/FileStatsHelper.kt
@@ -72,18 +72,15 @@ object FileStatsHelper {
         // Compute on IO thread
         val stats = withContext(Dispatchers.IO) {
             val file = FileUtils.getCacheFile(item, cacheDir)
-            if (file.exists()) {
-                try {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        val attrs = Files.readAttributes(file.toPath(), BasicFileAttributes::class.java)
-                        FileStats(true, attrs.size(), attrs.lastModifiedTime().toMillis())
-                    } else {
-                        FileStats(true, file.length(), file.lastModified())
-                    }
-                } catch (e: Exception) {
-                    FileStats(false, 0L, 0L)
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    val attrs = Files.readAttributes(file.toPath(), BasicFileAttributes::class.java)
+                    FileStats(true, attrs.size(), attrs.lastModifiedTime().toMillis())
+                } else {
+                    if (file.exists()) FileStats(true, file.length(), file.lastModified())
+                    else FileStats(false, 0L, 0L)
                 }
-            } else {
+            } catch (e: Exception) {
                 FileStats(false, 0L, 0L)
             }
         }
